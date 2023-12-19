530eb5e3660e29f3a1c820ceb631648e39fc39d5
sivaramsj <sivaramsj003@gmail.com>
2023-12-19 18:26:34 +0530

    form is created from the employee  models

---
+++ crudApp/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from crudApp.models import Employee
+
+
+class Employee_form(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = '__all__'
--- crudApp/views.py
+++ crudApp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from crudApp.models import Employee
+from crudApp.forms import Employee_form
 
 # Create your views here.
 def retrieve_list(request):
     employee=Employee.objects.all()
-    return render(request,'crudApp\employee_List.html',{'employee':employee})
+    return render(request,'crudApp/employee_List.html',{'employee':employee})
+
+def create_view(request):
+    form=Employee_form()
+    return render(request,'crudApp/create.html',{'form':form})
