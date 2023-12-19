a43c8bd91c7b098b13fbedab3831321e61a6f985
sivaramsj <sivaramsj003@gmail.com>
2023-12-19 17:46:08 +0530

    models (tables) are register in the admin

--- crudApp/admin.py
+++ crudApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from crudApp.models import Employee
 
 # Register your models here.
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list = ['emp_no', 'emp_name', 'emp_salary', 'emp_Address']
+
+admin.site.register(Employee,EmployeeAdmin)
