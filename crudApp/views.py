b46274cec6650b147bb5a6d726f4669d1f8633cf
sivaramsj <sivaramsj003@gmail.com>
2023-12-19 18:17:00 +0530

    Employee details are retrieve from the database

--- crudApp/views.py
+++ crudApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from crudApp.models import Employee
 
 # Create your views here.
+def retrieve_list(request):
+    employee=Employee.objects.all()
+    return render(request,'crudApp\employee_List.html',{'employee':employee})
