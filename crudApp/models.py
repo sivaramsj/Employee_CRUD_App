a320371dc0c733f6bf3ad3def324f2e60e1806dc
sivaramsj <sivaramsj003@gmail.com>
2023-12-19 17:40:20 +0530

    migrations is complete and table is created

--- crudApp/models.py
+++ crudApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    emp_no=models.IntegerField()
+    emp_name=models.CharField(max_length=20)
+    emp_salary=models.IntegerField()
+    emp_Address=models.CharField(max_length=50)
